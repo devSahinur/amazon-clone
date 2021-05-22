cc98a8f951b0d4ea7e4eaada6f1847fa5d1583a5
Sahinur Islam <infosahinur@gmail.com>
2021-05-22 17:34:51 +0600

    Next-auth complete

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,8 +4,13 @@ import {
     SearchIcon,
     ShoppingCartIcon,
  } from '@heroicons/react/outline';
+ import { signIn, signOut, useSession } from 'next-auth/client';
 
 function Header() {
+    const [session] = useSession();
+
+    
+
     return (
         <header>
             {/* Top Nav */}
@@ -26,8 +31,8 @@ function Header() {
                 </div>
                 {/* Right */}
                 <div className='text-white flex items-center text-xs space-x-6 mx-6 whitespace-nowrap'>
-                    <div  className='link'>
-                        <p>Hello Sahinur Islam</p>
+                    <div onClick={!session ? signIn : signOut} className='link'>
+                        {session ? `Hello, ${session.user.name}` : 'Sign In'}
                         <p className='font-extrabold md:text-sm'>Account & Lists</p>
                     </div>
                     <div className='link'>
